adb9751dfa83dbacfd00499593013e7300cb3436
yerasoni20 <yerasoni20@gmail.com>
2022-04-14 07:20:33 -0400

    Example of JMS

---
+++ src/main/java/com/example/javamessagingsystem/JavaMessagingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamessagingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMessagingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMessagingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javamessagingsystem/JavaMessagingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javamessagingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMessagingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
