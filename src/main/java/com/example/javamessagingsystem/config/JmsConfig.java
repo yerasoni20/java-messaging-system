bf123ff7bb3ca0404a0c0a25c73cabde107e4f00
yerasoni20 <yerasoni20@gmail.com>
2022-04-14 07:58:05 -0400

    adding message converter

---
+++ src/main/java/com/example/javamessagingsystem/config/JmsConfig.java
@@ -0,0 +1,22 @@
+package com.example.javamessagingsystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+/**
+ * Created by yera soni on 2022-04-14.
+ */
+@Configuration
+public class JmsConfig {
+
+    @Bean
+    public MessageConverter messageConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
+    }
+}
